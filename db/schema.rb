2eb984429245cc2c4cc60cdf4e365a05bd0e313e
theachyutkadam <theachyutkadam@gmail.com>
2017-10-04 15:15:51 +0530

    add mobile column in operator table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170930160527) do
+ActiveRecord::Schema.define(version: 20171004094224) do
 
   create_table "customers", force: :cascade do |t|
     t.string   "first_name"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20170930160527) do
     t.decimal  "salary"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
+    t.string   "mobile"
   end
 
   create_table "users", force: :cascade do |t|
