eda29134413e1d6c6ab5e3c98135c1539dff7e02
theachyutkadam <theachyutkadam@gmail.com>
2017-10-14 18:35:23 +0530

    show customer record with login_id

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    if current_user.id == 1
+    elsif current_user.id == 2
+       @cust = Customer.where(id: current_user.customer.id)
+       @products = Product.all
+    end
   end
 end
