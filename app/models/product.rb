0878ec692ecefee6d5170d39e643671b3de7e507
theachyutkadam <theachyutkadam@gmail.com>
2017-10-19 13:22:19 +0530

    validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
   belongs_to :customer
   belongs_to :operator
+
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :weight, presence: true
 end
