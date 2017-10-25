45c5bcda29c7e79beb8dc92404db40d42a0ce3f9
theachyutkadam <theachyutkadam@gmail.com>
2017-10-25 08:44:47 +0530

    operator details on show link

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ActiveRecord::Base
   belongs_to :customer
   belongs_to :operator
+
+  validates :product_name, presence: true
+  validates :price, presence: true
+  validates :weight, presence: true
+  validates :operator_id, presence: true
 end
