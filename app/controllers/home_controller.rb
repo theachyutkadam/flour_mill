2285d6552f70b656bca77aac549602903adec244
theachyutkadam <theachyutkadam@gmail.com>
2017-10-28 19:19:21 +0530

    design dashboard with css

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 class HomeController < ApplicationController
   def index
+
     if current_user.role.name == "Admin"
+      @customers = Customer.all
+      @operators = Operator.all
       # TODO Write a variable which only need for admin page.
     elsif current_user.role.name == "Customer"
       @products = Product.where(customer_id: current_user.customer.id)
