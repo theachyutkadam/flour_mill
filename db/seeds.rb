5a3317a0cf7ba8100b816757e31ed0fd83e76f03
ravikirankadam <ravikirankadam.1991@gmail.com>
2018-03-24 06:41:23 +0530

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Role Records
+Role.find_or_create_by(name: "Admin")
+Role.find_or_create_by(name: "Operator")
+Role.find_or_create_by(name: "Customer")
+
+# Admin User
+User.create(email: "admin@gmail.com", password: "12345678", role_id: 1)
+
+# Operator Users
+User.create(email: "ravikiran@gmail.com", password: "12345678", role_id: 2)
+User.create(email: "suryakant@gmail.com", password: "12345678", role_id: 2)
+User.create(email: "achyut@gmail.com", password: "12345678", role_id: 2)
+
+# Customers
+User.create(email: "ganesh@gmail.com", password: "12345678", role_id: 3)
+User.create(email: "sachin@gmail.com", password: "12345678", role_id: 3)
+User.create(email: "akash@gmail.com", password: "12345678", role_id: 3)
