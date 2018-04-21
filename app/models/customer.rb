f094977e9f55f717e316280413dafbe88e9fea98
AK <theachyutkadam@gmail.com>
2018-04-21 17:42:49 +0530

    removed customer validation

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,9 +4,6 @@ class Customer < ActiveRecord::Base
 	belongs_to :user
 
 	validates :first_name, presence: true
-	validates :middle_name, presence: true
 	validates :last_name, presence: true
   validates :address, presence: true
-  validates :mobile_num, presence: true, length: { minimum: 10 }
-  validates :mail, presence: true
 end
