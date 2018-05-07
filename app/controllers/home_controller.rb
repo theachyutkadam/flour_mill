302798a191125a0304ce29f967d99a099e5ba28c
theachyutkadam <theachyutkadam@gmail.com>
2018-05-07 17:59:37 +0530

    show customer & operator name ASC type

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,8 @@ class HomeController < ApplicationController
   def index
 
     if current_user.role.name == "Admin"
-      @customers = Customer.all
-      @operators = Operator.all
+      @customers = Customer.order(last_name: :asc)
+      @operators = Operator.order(last_name: :asc)
       # TODO Write a variable which only need for admin page.
     elsif current_user.role.name == "Customer"
       @products = Product.where(customer_id: current_user.customer.id)
