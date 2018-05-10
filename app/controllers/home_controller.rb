a498adbc907c3cebcd91a56472b9a2d89c4540f7
theachyutkadam <theachyutkadam@gmail.com>
2018-05-10 18:31:12 +0530

    add color navbar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,16 +9,11 @@ class HomeController < ApplicationController
         @customers = Customer.order(last_name: :asc)
         @operators = Operator.order(last_name: :asc)
       end
-
-
-
       # TODO Write a variable which only need for admin page.
     elsif current_user.role.name == "Customer"
       @products = Product.where(customer_id: current_user.customer.id).order(created_at: :desc)
-
-
-      
       @payments = Payment.where(customer_id: current_user.customer.id).order(created_at: :desc)
+
     elsif current_user.role.name == "Operator"
       if params[:search]
         @customers = Customer.where("first_name like ? OR last_name like ? OR mobile_num like ?", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%")
