a4d2999c249db1e572b3cb4a8429728d431f4c6d
ravikirankadam <ravikirankadam.1991@gmail.com>
2018-05-11 04:38:07 +0530

    Added log while seeding data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,25 @@
 # Role Records
+puts "Creating Role..."
 Role.find_or_create_by(name: "Admin")
 Role.find_or_create_by(name: "Operator")
 Role.find_or_create_by(name: "Customer")
+puts "Role created successfully"
 
 # Admin User
+puts "Creating User..."
 User.create(email: "admin@gmail.com", password: "12345678", role_id: 1)
+puts "User created successfully"
 
 # Operator Users
+puts "Creating Operator..."
 User.create(email: "ravikiran@gmail.com", password: "12345678", role_id: 2)
 User.create(email: "suryakant@gmail.com", password: "12345678", role_id: 2)
 User.create(email: "achyut@gmail.com", password: "12345678", role_id: 2)
+puts "Operator created successfully"
 
 # Customers
+puts "Creating customer..."
 User.create(email: "ganesh@gmail.com", password: "12345678", role_id: 3)
 User.create(email: "sachin@gmail.com", password: "12345678", role_id: 3)
 User.create(email: "akash@gmail.com", password: "12345678", role_id: 3)
+puts "Created customer successfully"
