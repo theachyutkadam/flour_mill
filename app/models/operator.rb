c1c86b821a689dcd396fb0c399b44554782c1eb5
theachyutkadam <theachyutkadam@gmail.com>
2018-05-12 17:36:42 +0530

    add search defination for navbar search

--- app/models/operator.rb
+++ app/models/operator.rb
@@ -9,4 +9,13 @@ class Operator < ActiveRecord::Base
   validates :mail, presence: true, :uniqueness => { scope: :user_id }, on: :create, :format => { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
   validates :permanent_address, presence: true, format: {with: /[a-z0-9]/, message: 'Write only small letter'}
   validates :salary, presence: true, numericality: true
+
+	def self.search(options)
+		if options[:search]
+      @operators = Operator.where("first_name like ? OR last_name like ? OR mobile like ?", "%#{options[:search]}%", "%#{options[:search]}%", "%#{options[:search]}%").order(last_name: :asc)
+    else
+      @operators = Operator.order(last_name: :asc)
+    end
+	end
+
 end
