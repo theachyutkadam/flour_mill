230a8370f26ece3893e1a8b1d813fb0c3769e82f
theachyutkadam <theachyutkadam@gmail.com>
2018-05-12 17:39:29 +0530

    remove search code and make another def of this search code in model.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,26 +1,14 @@
 class HomeController < ApplicationController
   def index
 
-    if current_user.role.name == "Admin"
-      if params[:search]
-        @customers = Customer.where("first_name like ? OR last_name like ? OR mobile_num like ?", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%").order(last_name: :asc)
-        @operators = Operator.where("first_name like ? OR last_name like ? OR mobile like ?", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%").order(last_name: :asc)
-      else
-        @customers = Customer.order(last_name: :asc)
-        @operators = Operator.order(last_name: :asc)
-      end
-      # TODO Write a variable which only need for admin page.
-    elsif current_user.role.name == "Customer"
+    if current_user.admin?
+      @customers = Customer.search(params)
+      @operators = Operator.search(params)
+    elsif current_user.customer?
       @products = Product.where(customer_id: current_user.customer.id).order(created_at: :desc)
       @payments = Payment.where(customer_id: current_user.customer.id).order(created_at: :desc)
-
-    elsif current_user.role.name == "Operator"
-      if params[:search]
-        @customers = Customer.where("first_name like ? OR last_name like ? OR mobile_num like ?", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%")
-      else
-        @customers = Customer.order(last_name: :asc)
-      end
-      # TODO Write a variable which only need for operator page.
+    elsif current_user.operator?
+      @customers = Customer.search(params)
     end
   end
 end
