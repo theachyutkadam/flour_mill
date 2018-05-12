e56dd831eb36f69e97aefd90f78d8c5c5526ee31
theachyutkadam <theachyutkadam@gmail.com>
2018-05-13 01:00:29 +0530

    make TINY controller & FAT model of operator

--- app/models/operator.rb
+++ app/models/operator.rb
@@ -1,5 +1,5 @@
 class Operator < ActiveRecord::Base
-	has_many :products
+  has_many :products
   belongs_to :user
 
   validates :first_name, presence: true, format: {with: /[a-z]/, message: 'Write only small letter'}
@@ -10,12 +10,27 @@ class Operator < ActiveRecord::Base
   validates :permanent_address, presence: true, format: {with: /[a-z0-9]/, message: 'Write only small letter'}
   validates :salary, presence: true, numericality: true
 
-	def self.search(options)
-		if options[:search]
+  before_create :create_user
+  before_update :update_user
+
+  def self.search(options)
+    if options[:search]
       @operators = Operator.where("first_name like ? OR last_name like ? OR mobile like ?", "%#{options[:search]}%", "%#{options[:search]}%", "%#{options[:search]}%").order(last_name: :asc)
     else
       @operators = Operator.order(last_name: :asc)
     end
-	end
+  end
 
+  def create_user
+    @user = User.new
+    @user.email = self.mail
+    @user.password = mobile
+    @user.role_id = Role.find_by_name("Operator").id
+    @user.save
+    self.user_id = user.id
+  end
+
+  def update_user
+    self.user.update(email: self.mail, password: mobile)
+  end
 end
