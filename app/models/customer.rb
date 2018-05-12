83d6eea117e6f8241a73d72179cf109859d3ae53
theachyutkadam <theachyutkadam@gmail.com>
2018-05-12 16:49:54 +0530

    Added before update callbacks and minor code refactored

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -9,14 +9,33 @@ class Customer < ActiveRecord::Base
   validates :mail, :uniqueness => { scope: :user_id }, on: :create
   validates :mobile_num, numericality: true, :length => { minimum: 10 }, uniqueness: true, on: :create
   before_create :create_user
+  before_update :update_user
+
+  def self.search(options)
+    if options[:search]
+      Customer.where("first_name like ? OR last_name like ? OR mobile_num like ?", "%#{options[:search]}%", "%#{options[:search]}%", "%#{options[:search]}%")
+    else
+      Customer.order(last_name: :asc)
+    end
+  end
 
   def create_user
-    self.mail = first_name + mobile_num.to_s.last(4)
+    if self.mail.blank?
+      self.mail = generate_username
+    end
     user = User.new
     user.email = self.mail
     user.password = mobile_num
     user.role_id = Role.find_by_name("Customer").id
     user.save
     self.user_id = user.id
+  end
+
+  def generate_username
+    first_name + mobile_num.to_s.last(4)
+  end
+
+  def update_user
+    self.user.update(email: self.mail, password: mobile_num)
   end
 end
