bab4b543ba08820a1ce57976a33b31bab7500ce7
theachyutkadam <theachyutkadam@gmail.com>
2018-05-13 22:02:25 +0530

    manage enter record only one admin login

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,19 +7,19 @@ puts "Role created successfully"
 
 # Admin User
 puts "Creating User..."
-User.create(email: "admin@gmail.com", password: "12345678", role_id: 1)
+User.create(email: "admin@gmail.com", password: "password", role_id: 1)
 puts "User created successfully"
 
 # Operator Users
-puts "Creating Operator..."
-User.create(email: "ravikiran@gmail.com", password: "12345678", role_id: 2)
-User.create(email: "suryakant@gmail.com", password: "12345678", role_id: 2)
-User.create(email: "achyut@gmail.com", password: "12345678", role_id: 2)
-puts "Operator created successfully"
+# puts "Creating Operator..."
+# User.create(email: "ravikiran@gmail.com", password: "9158308765", role_id: 2)
+# User.create(email: "suryakant@gmail.com", password: "9673151454", role_id: 2)
+# User.create(email: "achyut@gmail.com", password: "9607180726", role_id: 2)
+# puts "Operator created successfully"
 
 # Customers
-puts "Creating customer..."
-User.create(email: "ganesh@gmail.com", password: "12345678", role_id: 3)
-User.create(email: "sachin@gmail.com", password: "12345678", role_id: 3)
-User.create(email: "akash@gmail.com", password: "12345678", role_id: 3)
-puts "Created customer successfully"
+# puts "Creating customer..."
+# User.create(email: "ganesh@gmail.com", password: "12345678", role_id: 3)
+# User.create(email: "sachin@gmail.com", password: "12345678", role_id: 3)
+# User.create(email: "akash@gmail.com", password: "12345678", role_id: 3)
+# puts "Created customer successfully"
