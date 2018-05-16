5c202baa2207d6e6d9394b49e700d2cf0fd52ecb
ravikirankadam <ravikirankadam.1991@gmail.com>
2018-05-17 03:32:06 +0530

    Added Logger while running tests

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -7,6 +7,10 @@ Rails.application.configure do
   # and recreated between test runs. Don't rely on the data there!
   config.cache_classes = true
 
+  # Added log while running tests
+  config.logger = Logger.new(STDOUT)
+  config.logger.level = Logger::ERROR
+
   # Do not eager load code on boot. This avoids loading your whole application
   # just for the purpose of running a single test. If you are using a tool that
   # preloads Rails for running tests, you may have to set it to true.
