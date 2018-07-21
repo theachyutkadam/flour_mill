eb60c18202c5c823a8df563af2193db8befbbdad
theachyutkadam <theachyutkadam@gmail.com>
2018-07-22 03:37:26 +0530

    show rate & price of oninpute event

--- app/assets/javascripts/product.js
+++ app/assets/javascripts/product.js
@@ -1,10 +1,23 @@
 function setPrice() {
   var weight = document.getElementById("weight").value;
-  if(weight == "") {
+  var rate = document.getElementById("rate").value;
+  if(weight == "" && rate == "") {
     document.getElementById("price").innerHTML = "Rs. 00";
+    // document.getElementById("rate").innerHTML = "Rs. 00";
   }
   else {
-    var price = parseFloat(weight)*3;
+    var price = parseFloat(weight) * parseFloat(rate);
     document.getElementById("price").innerHTML = "Rs. " +price;
   }
+}
+
+function showRate() {
+  var item_id = document.getElementById("product_product_name").value;
+  url = "/items/" + item_id + ".json"
+  $.ajax({url: url, success: function(result){
+    console.log(result.item.rate);
+    rate = result.item.rate
+    document.getElementById("rate").value = rate;
+   }
+  });
 }
