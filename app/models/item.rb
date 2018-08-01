e8daf4d06c58c6682b58143a88c0ea08199d0a5d
theachyutkadam <theachyutkadam@gmail.com>
2018-08-01 14:20:02 +0530

    add num valildation and some changes

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,5 @@ class Item < ActiveRecord::Base
   # uniqueness: true, on: :create
   has_many :products
   validates :item_name, presence: true, uniqueness: true, on: :create, format: {with: /[a-z]/, message: 'Write only small letter'}
-  validates :rate, presence: true
+  validates :rate, presence: true, numericality: true
 end
