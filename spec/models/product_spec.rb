18fe9b34b4c132aaa0678caaf86cb0c447edc7b9
theachyutkadam <theachyutkadam@gmail.com>
2018-08-08 12:29:58 +0530

    mobile number make 10 digits

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -8,8 +8,8 @@ RSpec.describe Product, type: :model do
       operator_role = Role.create(name: "Operator")
       customer_user = User.create(email: "customer@gmail.com", password: "password", role_id: customer_role.id)
       operator_user = User.create(email: "operator@gmail.com", password: "password", role_id: operator_role.id)
-      @customer = Customer.create(first_name: "name", last_name: "lastname", mobile_num: "12345678790", family_members: 5, address: "saykhind", user_id: customer_user.id)
-      @operator = Operator.create(first_name: "name", middle_name: "middlename", last_name: "lastname", mobile: "12345678790", permanent_address: "saykhind", mail: "name@gmail.com", salary: 5000, user_id: operator_user.id)
+      @customer = Customer.create(first_name: "name", last_name: "lastname", mobile_num: "1234567890", family_members: 5, address: "saykhind", user_id: customer_user.id)
+      @operator = Operator.create(first_name: "name", middle_name: "middlename", last_name: "lastname", mobile: "1234567890", permanent_address: "saykhind", mail: "name@gmail.com", salary: 5000, user_id: operator_user.id)
       @product = Product.create(product_name: "bajari", price: 10, weight: 5, customer_id: @customer.id, operator_id: @operator.id, payment_type: "cash")
     end
 
@@ -29,8 +29,8 @@ RSpec.describe Product, type: :model do
         operator_role = Role.create(name: "Operator")
         customer_user = User.create(email: "customer@gmail.com", password: "password", role_id: customer_role.id)
         operator_user = User.create(email: "operator@gmail.com", password: "password", role_id: operator_role.id)
-        customer = Customer.create(first_name: "name", last_name: "lastname", mobile_num: "12345678790", family_members: 5, address: "saykhind", user_id: customer_user.id)
-        operator = Operator.create(first_name: "name", middle_name: "middlename", last_name: "lastname", mobile: "12345678790", permanent_address: "saykhind", mail: "name@gmail.com", salary: 5000, user_id: operator_user.id)
+        customer = Customer.create(first_name: "name", last_name: "lastname", mobile_num: "1234567890", family_members: 5, address: "saykhind", user_id: customer_user.id)
+        operator = Operator.create(first_name: "name", middle_name: "middlename", last_name: "lastname", mobile: "1234567890", permanent_address: "saykhind", mail: "name@gmail.com", salary: 5000, user_id: operator_user.id)
         product = Product.new(product_name: nil, price: 10, weight: 5, customer_id: customer.id, operator_id: operator.id, payment_type: "cash")
         product.save
         expect(product.errors.full_messages.first).to eq("Product name can't be blank")
@@ -41,8 +41,8 @@ RSpec.describe Product, type: :model do
         operator_role = Role.create(name: "Operator")
         customer_user = User.create(email: "customer@gmail.com", password: "password", role_id: customer_role.id)
         operator_user = User.create(email: "operator@gmail.com", password: "password", role_id: operator_role.id)
-        customer = Customer.create(first_name: "name", last_name: "lastname", mobile_num: "12345678790", family_members: 5, address: "saykhind", user_id: customer_user.id)
-        operator = Operator.create(first_name: "name", middle_name: "middlename", last_name: "lastname", mobile: "12345678790", permanent_address: "saykhind", mail: "name@gmail.com", salary: 5000, user_id: operator_user.id)
+        customer = Customer.create(first_name: "name", last_name: "lastname", mobile_num: "1234567890", family_members: 5, address: "saykhind", user_id: customer_user.id)
+        operator = Operator.create(first_name: "name", middle_name: "middlename", last_name: "lastname", mobile: "1234567890", permanent_address: "saykhind", mail: "name@gmail.com", salary: 5000, user_id: operator_user.id)
         product = Product.new(product_name: 12345, price: 10, weight: 5, customer_id: customer.id, operator_id: operator.id, payment_type: "cash")
         product.save
         expect(product.errors.full_messages.first).to eq("Product name Write only small letter")
