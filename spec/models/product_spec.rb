ea6c7dd029bf0d5e27f4bb2f1dfcd47aaf461876
theachyutkadam <theachyutkadam@gmail.com>
2018-08-09 15:31:00 +0530

    remove one test case of validation

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -20,6 +20,10 @@ RSpec.describe Product, type: :model do
     it "should belongs to operator" do
       expect(@operator).to eq(@product.operator)
     end
+
+    it "should belongs to item" do
+      expect(@item).to eq(@product.item)
+    end
   end
 
   context "validation" do
@@ -36,17 +40,6 @@ RSpec.describe Product, type: :model do
         expect(product.errors.full_messages.first).to eq("Product name can't be blank")
       end
 
-      it "gives validation error on characters only" do
-        customer_role = Role.create(name: "Customer")
-        operator_role = Role.create(name: "Operator")
-        customer_user = User.create(email: "customer@gmail.com", password: "password", role_id: customer_role.id)
-        operator_user = User.create(email: "operator@gmail.com", password: "password", role_id: operator_role.id)
-        customer = Customer.create(first_name: "name", last_name: "lastname", mobile_num: "1234567890", family_members: 5, address: "saykhind", user_id: customer_user.id)
-        operator = Operator.create(first_name: "name", middle_name: "middlename", last_name: "lastname", mobile: "1234567890", permanent_address: "saykhind", mail: "name@gmail.com", salary: 5000, user_id: operator_user.id)
-        product = Product.new(product_name: 12345, price: 10, weight: 5, customer_id: customer.id, operator_id: operator.id, payment_type: "cash")
-        product.save
-        expect(product.errors.full_messages.first).to eq("Product name Write only small letter")
-      end
     end
   end
 end
