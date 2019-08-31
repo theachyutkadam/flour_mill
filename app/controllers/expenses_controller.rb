8f6b032128794e44486ae2e1105847631c423d9c
achyut <theachyutkadam@gmail.com>
2019-09-01 04:55:25 +0530

    indentation setup

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,45 +1,26 @@
 class ExpensesController < ApplicationController
+  before_action:, only: [:edit_exp, :update_exp, :destroy_exp]
   def index
     @expenses = Expense.all
     @exp = Expense.new
   end
 
   def create_expense
-    form = params[:expenditure]
-    @exp = Expense.new
-
-    @exp.expence_type = form[:expense_type]
-    @exp.expence = form[:expense].to_f
-    @exp.quantity = form[:quantity]
-    @exp.description = form[:description]
+    @exp = Expense.new(expense_params)
     if @exp.save
       flash[:notice] = 'Expense was successfully Created.'
       redirect_to expenses_path
     else
       @expenses = Expense.all
-      # @exp.errors.messages
       flash[:alert] = 'Expense not Create.'
       render :index
     end
   end
 
-  def edit_exp
-    @exp = Expense.find(params[:id])
-  end
-
-  def destroy_exp
-    @exp = Expense.find(params[:id])
-    @exp.destroy
-    redirect_to expenses_path
-  end
+  def edit_exp; end
 
   def update_exp
-    form = params[:edit_exp]
-    @exp = Expense.find(form[:id])
-    @exp.expence_type = form[:expense_type]
-    @exp.expence = form[:expense]
-    @exp.quantity = form[:quantity]
-    if @exp.save
+    if @exp.update(expense_params)
       flash[:notice] = 'Expense was successfully updated.'
       redirect_to expenses_path
     else
@@ -48,9 +29,17 @@ class ExpensesController < ApplicationController
     end
   end
 
+  def destroy_exp
+    @exp.destroy
+    redirect_to expenses_path
+  end
+
   private
   def expense_params
     params.require(:expense).permit(:expense_type, :expence, :quantity, :description)
+  end
+  def find_expenses
+    @exp = Expense.find(params[:id])
   end
 
 end
